9768466260b525f0c4dca63668bb4c8324600882
stijnteunissen <stijnteunissen@ziggo.nl>
2025-04-23 11:16:55 +0200

    update

--- R/tax_clean.R
+++ R/tax_clean.R
@@ -12,7 +12,7 @@ tax_clean = function(physeq = physeq, tax_filter = TRUE) {
   uncleaned_phyloseq <- readRDS(uncleaned_file)
   cleaned_phyloseq   <- readRDS(cleaned_file)
 
-  keep_ranks <- c("Family", "Genus", "Species")
+  keep_ranks <- c("Family", "Genus")
 
   subset_tax_table <- function(ps) {
     tax = tax_table(ps)
@@ -29,14 +29,12 @@ tax_clean = function(physeq = physeq, tax_filter = TRUE) {
     "2b68abfcc52aa30e49215e5a7d0657dd",
     "bca1ec8c1c08341078a4a81852aec77c",
     "3203a668b6213d8fb17701f535e72334",
-    "6a6110b7e3b01c8b2143982d71739c5b"
-  )
+    "6a6110b7e3b01c8b2143982d71739c5b")
+
   uncleaned_sel <- prune_taxa(selected_otus, uncleaned_phyloseq)
   cleaned_sel   <- prune_taxa(selected_otus, cleaned_phyloseq)
 
-  message("== Uncleaned, geselecteerde ASVs ==")
   print(tax_table(uncleaned_sel))
-  message("== Cleaned, geselecteerde ASVs ==")
   print(tax_table(cleaned_sel))
 
   new_folder    <- file.path(base_path, projects, "output_data/rds_files/Before_cleaning_rds_files")
